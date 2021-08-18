bf10a8bc9b410c3f7165f1482a041e2770794fbe
Mahaoui Abdennasser <7na2ro@gmail.com>
2021-08-18 21:27:18 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
-function changeBills(cash) {
-  if (cash >= 10) {
-    let ten = Math.floor(cash / 10);
-    let five =
-      Math.floor(cash % 10) >= 5 ? Math.floor(Math.floor(cash % 10) / 5) : 0;
-    let two =
-      five >= 2 || five <= 1 ? Math.floor(Math.floor(cash % 10) % 2) : 0;
-    return {
-      two,
-      five,
-      ten,
-    };
-  } else if (cash < 10 && cash >= 5) {
-    let ten = 0;
-    let five = Math.floor(cash / 5);
-    let two = five == 1 ? Math.floor(Math.floor(cash % 5) / 2) : 0;
-    return {
-      ten,
-      five,
-      two,
-    };
-  } else {
-    let ten = 0;
-    let five = 0;
-    let two = Math.floor(cash / 2) != 0 ? Math.floor(cash / 2) : 0;
-    return {
-      ten,
-      five,
-      two,
-    };
-  }
-
-  // code
+const changeBills = (cash) => {
+  const ten = Math.floor(cash / 10);
+  const five = Math.floor(Math.floor(cash % 10) / 5);
+  const two = Math.floor(Math.floor(cash % 5) / 2);
   return {
     two,
     five,
     ten,
   };
-}
+};
 
 console.log("changeBills(17).ten :", changeBills(17).ten);
 console.log("changeBills(17).five :", changeBills(17).five);
